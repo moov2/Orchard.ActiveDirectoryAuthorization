34c178a52d0c88c4228a5841a1068c99013400fb
kevinsmgov <kevin.hobson@smgov.net>
2016-04-13 07:43:25 -0700

    update to work with 1.10.x

--- Services/ActiveDirectoryAuthorizationService.cs
+++ Services/ActiveDirectoryAuthorizationService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using JetBrains.Annotations;
 using Orchard.Localization;
 using Orchard.Logging;
 using Orchard.ContentManagement;
